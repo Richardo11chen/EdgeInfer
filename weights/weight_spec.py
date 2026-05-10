4539a0efb1ec49a9c15d63729360a50736ac7ace
Aromatic <symwww@outlook.com>
2026-05-10 21:05:50 +0800

    feat(weights): add weight key constants

--- weights/weight_spec.py
+++ weights/weight_spec.py
@@ -6,6 +6,25 @@ from collections.abc import Iterable, Mapping
 import torch
 
 
+LAYER_WEIGHT_KEYS: tuple[str, ...] = (
+    "input_layernorm.weight",
+    "self_attn.q_proj.weight",
+    "self_attn.k_proj.weight",
+    "self_attn.v_proj.weight",
+    "self_attn.o_proj.weight",
+    "post_attention_layernorm.weight",
+    "mlp.gate_proj.weight",
+    "mlp.up_proj.weight",
+    "mlp.down_proj.weight",
+)
+
+GLOBAL_WEIGHT_KEYS: tuple[str, ...] = (
+    "model.embed_tokens.weight",
+    "model.norm.weight",
+    "lm_head.weight",
+)
+
+
 @dataclass(frozen=True)
 class LayerWeights:
     layer_id: int
